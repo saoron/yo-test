fa8a226ebd2c4b5b3364011f67d8aaaafeff1380
saoron <saoron@gmail.com>
2020-03-31 12:40:32 +0300

    add file download to test

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const request = require('request');
 const colors = require('colors');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const baseUrl = 'http://192.168.0.1:12913/';
 let successCount = 0;
 
 // configs
 const totalTests = 10;
-const DEBUG = false;
+const DEBUG = true;
 
 main();
 
 async function main() {
+  //clear old downloaded videos
+  clearOutPutDir();
+
   for (var i = 0; i < totalTests; i++) {
     try {
       await testStartRecord();
@@ -49,7 +55,7 @@ function testStartRecord() {
 
     setTimeout(() => {
       getClips().then(
-        res => {
+        async res => {
           if (res['error']) {
             console.log(colors.red('test /startRecord: ✘'), res['error']);
             reject(res['error']);
@@ -62,6 +68,12 @@ function testStartRecord() {
           if (res['clips'][0].url) {
             console.warn(colors.green('test /startRecord: ✔'));
             console.log(colors.yellow(res['clips'][0].url));
+            try {
+              await downloadFile(res['clips'][0].url);
+            } catch (e) {
+              console.log(colors.red('Download Failed'));
+              return;
+            }
           } else {
             console.warn(colors.red('test /startRecord: ✘'));
           }
@@ -147,5 +159,40 @@ function verifyTheresNoClipsInTheSystem() {
         }
       }
     });
+  });
+}
+
+function downloadFile(url) {
+  return new Promise((resolve, reject) => {
+    var file = fs.createWriteStream(
+      process.env.PWD + '/output/' + url.split('/').pop()
+    );
+    var request = http
+      .get(url, function(response) {
+        response.pipe(file);
+        file.on('finish', function() {
+          if (DEBUG) console.log('Download complete');
+          file.close();
+          resolve();
+        });
+      })
+      .on('error', function(err) {
+        // Handle errors
+        if (DEBUG) console.log(colors.red('Download error', err));
+        fs.unlink(process.env.PWD + '/output/' + url.split('/').pop()); // Delete the file async. (But we don't check the result)
+        reject(err);
+      });
+  });
+}
+
+function clearOutPutDir() {
+  fs.readdir(process.env.PWD + '/output/', (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlink(path.join(process.env.PWD + '/output/', file), err => {
+        if (err) throw err;
+      });
+    }
   });
 }
