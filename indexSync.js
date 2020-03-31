8d92e6b08679feb1ca04d04d8832093ea986c332
Yossi Neiman <saoron@gmail.com>
2020-03-31 13:38:06 +0300

    add startRecordSync test

--- indexSync.js
+++ indexSync.js
@@ -36,11 +36,42 @@ function testStartRecord() {
   return new Promise((resolve, reject) => {
     startRecord().then(
       res => {
-        if (DEBUG) console.warn(res);
+        if (DEBUG) console.warn("startRecordSync resolved!", res);
         if (res.status == "failed") {
           console.error("Test Failed: recording_init_failed");
           reject("recording_init_failed");
           return;
+        } else {
+          getClips().then(
+            async res => {
+              if (res["error"]) {
+                console.log(colors.red("test /startRecord: ✘"), res["error"]);
+                reject(res["error"]);
+                return;
+              }
+
+              if (res["clips"][0].url) {
+                successCount++;
+              }
+              if (res["clips"][0].url) {
+                console.warn(colors.green("test /startRecord: ✔"));
+                console.log(colors.yellow(res["clips"][0].url));
+                try {
+                  await downloadFile(res["clips"][0].url);
+                } catch (e) {
+                  console.log(colors.red("Download Failed"));
+                  return;
+                }
+              } else {
+                console.warn(colors.red("test /startRecord: ✘"));
+              }
+              resolve();
+            },
+            err => {
+              console.log(colors.green("test /startRecord:"), err);
+              reject(err);
+            }
+          );
         }
       },
       err => {
@@ -52,39 +83,6 @@ function testStartRecord() {
         }
       }
     );
-
-    setTimeout(() => {
-      getClips().then(
-        async res => {
-          if (res["error"]) {
-            console.log(colors.red("test /startRecord: ✘"), res["error"]);
-            reject(res["error"]);
-            return;
-          }
-
-          if (res["clips"][0].url) {
-            successCount++;
-          }
-          if (res["clips"][0].url) {
-            console.warn(colors.green("test /startRecord: ✔"));
-            console.log(colors.yellow(res["clips"][0].url));
-            try {
-              await downloadFile(res["clips"][0].url);
-            } catch (e) {
-              console.log(colors.red("Download Failed"));
-              return;
-            }
-          } else {
-            console.warn(colors.red("test /startRecord: ✘"));
-          }
-          resolve();
-        },
-        err => {
-          console.log(colors.green("test /startRecord:"), err);
-          reject(err);
-        }
-      );
-    }, 70000);
   });
 }
 
@@ -106,58 +104,17 @@ function getClips() {
 
 function startRecord() {
   return new Promise((resolve, reject) => {
-    request(baseUrl + "startRecord", { json: true }, (err, res, body) => {
+    request(baseUrl + "startRecordSync", { json: true }, (err, res, body) => {
       if (err) {
         console.error(err);
         reject(err);
         return;
       }
 
-      //after we start record verify no clips in the system
-      setTimeout(() => {
-        verifyTheresNoClipsInTheSystem().then(
-          res => {
-            if (res) {
-              if (DEBUG) console.log("Tested for: No clips in the system ✔");
-            } else {
-              console.error("Test Failed");
-              reject();
-              return;
-            }
-          },
-          e => {
-            console.error("Test Failed");
-            console.error(e);
-            reject();
-            return;
-          }
-        );
-      }, 10 * 1000);
-
       if (body.error) {
         reject(body.error);
       } else {
         resolve(body);
-      }
-    });
-  });
-}
-
-function verifyTheresNoClipsInTheSystem() {
-  return new Promise((resolve, reject) => {
-    request(baseUrl + "getClips", { json: true }, (err, res, body) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      }
-      if (res.error) {
-        reject(body.error);
-      } else {
-        if (body.error == "no_dir") {
-          resolve(true);
-        } else {
-          resolve(false);
-        }
       }
     });
   });
