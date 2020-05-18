fa632b730f08a821fdb45e432897e4dd7e53ede9
saoron <saoron@gmail.com>
2020-05-18 13:24:37 +0300

    add start & stop stream

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const request = require("request");
-const colors = require("colors");
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const request = require('request');
+const colors = require('colors');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const baseUrl = "http://192.168.0.1:12913/";
+const baseUrl = 'http://192.168.0.1:12913/';
 let successCount = 0;
 
 // configs
-const totalTests = 10;
+const totalTests = 1;
 const DEBUG = true;
+var sessionId = null;
 
 main();
 
@@ -19,34 +20,48 @@ async function main() {
 
   for (var i = 0; i < totalTests; i++) {
     try {
+      //startStream
+      console.warn('startStream..');
+      await startStream();
+      //wait 20 seconds
+      console.warn('startStream..Sleep 20');
+      await sleep(20);
+      //stopStream
+      console.warn('stopStream');
+      await stopStream();
+
+      //wait 10 seconds
+      await sleep(10);
+
       await testStartRecord();
+      await sleep();
     } catch (e) {
       console.log(colors.red(e));
     }
   }
-  console.warn("********************************");
+  console.warn('********************************');
   console.warn(
-    "******Done testing " + successCount + "/" + totalTests + " *********"
+    '******Done testing ' + successCount + '/' + totalTests + ' *********'
   );
-  console.warn("********************************");
+  console.warn('********************************');
 }
 
 // test 1 /startRecord
 function testStartRecord() {
   return new Promise((resolve, reject) => {
     startRecord().then(
-      res => {
+      (res) => {
         if (DEBUG) console.warn(res);
-        if (res.status == "failed") {
-          console.error("Test Failed: recording_init_failed");
-          reject("recording_init_failed");
+        if (res.status == 'failed') {
+          console.error('Test Failed: recording_init_failed');
+          reject('recording_init_failed');
           return;
         }
       },
-      err => {
+      (err) => {
         if (DEBUG) console.error(err);
 
-        if (err == "recording_init_failed") {
+        if (err == 'recording_init_failed') {
           reject(err);
           return;
         }
@@ -55,32 +70,32 @@ function testStartRecord() {
 
     setTimeout(() => {
       getClips().then(
-        async res => {
-          if (res["error"]) {
-            console.log(colors.red("test /startRecord: ✘"), res["error"]);
-            reject(res["error"]);
+        async (res) => {
+          if (res['error']) {
+            console.log(colors.red('test /startRecord: ✘'), res['error']);
+            reject(res['error']);
             return;
           }
 
-          if (res["clips"][0].url) {
+          if (res['clips'][0].url) {
             successCount++;
           }
-          if (res["clips"][0].url) {
-            console.warn(colors.green("test /startRecord: ✔"));
-            console.log(colors.yellow(res["clips"][0].url));
+          if (res['clips'][0].url) {
+            console.warn(colors.green('test /startRecord: ✔'));
+            console.log(colors.yellow(res['clips'][0].url));
             try {
-              await downloadFile(res["clips"][0].url);
+              await downloadFile(res['clips'][0].url);
             } catch (e) {
-              console.log(colors.red("Download Failed"));
+              console.log(colors.red('Download Failed'));
               return;
             }
           } else {
-            console.warn(colors.red("test /startRecord: ✘"));
+            console.warn(colors.red('test /startRecord: ✘'));
           }
           resolve();
         },
-        err => {
-          console.log(colors.green("test /startRecord:"), err);
+        (err) => {
+          console.log(colors.green('test /startRecord:'), err);
           reject(err);
         }
       );
@@ -90,7 +105,7 @@ function testStartRecord() {
 
 function getClips() {
   return new Promise((resolve, reject) => {
-    request(baseUrl + "getClips", { json: true }, (err, res, body) => {
+    request(baseUrl + 'getClips', { json: true }, (err, res, body) => {
       if (err) {
         console.error(err);
         reject(err);
@@ -104,9 +119,57 @@ function getClips() {
   });
 }
 
+function startStream() {
+  return new Promise((resolve, reject) => {
+    request(baseUrl + 'startStream', { json: true }, (err, res, body) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      }
+
+      if (body && body.error) {
+        console.error(body);
+        reject(body.error);
+      } else {
+        console.log(body);
+        sessionId = body.sessionId;
+        resolve(body);
+      }
+    });
+  });
+}
+function stopStream() {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        uri: baseUrl + 'stopStream',
+        json: true,
+        headers: {
+          SessionId: sessionId,
+        },
+        form: { data: {} },
+
+        method: 'POST',
+      },
+      (err, res, body) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        }
+        if (body && body.error) {
+          reject(body.error);
+        } else {
+          sessionId = body.sessionId;
+          resolve(body);
+        }
+      }
+    );
+  });
+}
+
 function startRecord() {
   return new Promise((resolve, reject) => {
-    request(baseUrl + "startRecord", { json: true }, (err, res, body) => {
+    request(baseUrl + 'startRecord', { json: true }, (err, res, body) => {
       if (err) {
         console.error(err);
         reject(err);
@@ -116,17 +179,17 @@ function startRecord() {
       //after we start record verify no clips in the system
       setTimeout(() => {
         verifyTheresNoClipsInTheSystem().then(
-          res => {
+          (res) => {
             if (res) {
-              if (DEBUG) console.log("Tested for: No clips in the system ✔");
+              if (DEBUG) console.log('Tested for: No clips in the system ✔');
             } else {
-              console.error("Test Failed");
+              console.error('Test Failed');
               reject();
               return;
             }
           },
-          e => {
-            console.error("Test Failed");
+          (e) => {
+            console.error('Test Failed');
             console.error(e);
             reject();
             return;
@@ -145,7 +208,7 @@ function startRecord() {
 
 function verifyTheresNoClipsInTheSystem() {
   return new Promise((resolve, reject) => {
-    request(baseUrl + "getClips", { json: true }, (err, res, body) => {
+    request(baseUrl + 'getClips', { json: true }, (err, res, body) => {
       if (err) {
         console.error(err);
         reject(err);
@@ -153,7 +216,7 @@ function verifyTheresNoClipsInTheSystem() {
       if (res.error) {
         reject(body.error);
       } else {
-        if (body.error == "no_dir") {
+        if (body.error == 'no_dir') {
           resolve(true);
         } else {
           resolve(false);
@@ -166,34 +229,42 @@ function verifyTheresNoClipsInTheSystem() {
 function downloadFile(url) {
   return new Promise((resolve, reject) => {
     var file = fs.createWriteStream(
-      process.env.PWD + "/output/" + url.split("/").pop()
+      process.env.PWD + '/output/' + url.split('/').pop()
     );
     var request = http
-      .get(url, function(response) {
+      .get(url, function (response) {
         response.pipe(file);
-        file.on("finish", function() {
-          if (DEBUG) console.log("Download complete");
+        file.on('finish', function () {
+          if (DEBUG) console.log('Download complete');
           file.close();
           resolve();
         });
       })
-      .on("error", function(err) {
+      .on('error', function (err) {
         // Handle errors
-        if (DEBUG) console.log(colors.red("Download error", err));
-        fs.unlink(process.env.PWD + "/output/" + url.split("/").pop()); // Delete the file async. (But we don't check the result)
+        if (DEBUG) console.log(colors.red('Download error', err));
+        fs.unlink(process.env.PWD + '/output/' + url.split('/').pop()); // Delete the file async. (But we don't check the result)
         reject(err);
       });
   });
 }
 
 function clearOutPutDir() {
-  fs.readdir(process.env.PWD + "/output/", (err, files) => {
+  fs.readdir(process.env.PWD + '/output/', (err, files) => {
     if (err) throw err;
 
     for (const file of files) {
-      fs.unlink(path.join(process.env.PWD + "/output/", file), err => {
+      fs.unlink(path.join(process.env.PWD + '/output/', file), (err) => {
         if (err) throw err;
       });
     }
+  });
+}
+
+function sleep(seconds) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, seconds * 1000);
   });
 }
--- indexSync.js
+++ indexSync.js
@@ -1,10 +1,10 @@
-const request = require("request");
-const colors = require("colors");
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const request = require('request');
+const colors = require('colors');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const baseUrl = "http://192.168.0.1:12913/";
+const baseUrl = 'http://192.168.0.1:12913/';
 let successCount = 0;
 
 // configs
@@ -24,11 +24,11 @@ async function main() {
       console.log(colors.red(e));
     }
   }
-  console.warn("********************************");
+  console.warn('********************************');
   console.warn(
-    "******Done testing " + successCount + "/" + totalTests + " *********"
+    '******Done testing ' + successCount + '/' + totalTests + ' *********'
   );
-  console.warn("********************************");
+  console.warn('********************************');
 }
 
 // test 1 /startRecord
@@ -36,48 +36,50 @@ function testStartRecord() {
   return new Promise((resolve, reject) => {
     startRecord().then(
       res => {
-        if (DEBUG) console.warn("startRecordSync resolved!", res);
-        if (res.status == "failed") {
-          console.error("Test Failed: recording_init_failed");
-          reject("recording_init_failed");
+        if (DEBUG) console.warn('startRecordSync resolved!', res);
+        if (res.status == 'failed') {
+          console.error('Test Failed: recording_init_failed');
+          reject('recording_init_failed');
           return;
         } else {
-          getClips().then(
-            async res => {
-              if (res["error"]) {
-                console.log(colors.red("test /startRecord: ✘"), res["error"]);
-                reject(res["error"]);
-                return;
-              }
-
-              if (res["clips"][0].url) {
-                successCount++;
-              }
-              if (res["clips"][0].url) {
-                console.warn(colors.green("test /startRecord: ✔"));
-                console.log(colors.yellow(res["clips"][0].url));
-                try {
-                  await downloadFile(res["clips"][0].url);
-                } catch (e) {
-                  console.log(colors.red("Download Failed"));
+          setTimeout(() => {
+            getClips().then(
+              async res => {
+                if (res['error']) {
+                  console.log(colors.red('test /startRecord: ✘'), res['error']);
+                  reject(res['error']);
                   return;
                 }
-              } else {
-                console.warn(colors.red("test /startRecord: ✘"));
+
+                if (res['clips'][0].url) {
+                  successCount++;
+                }
+                if (res['clips'][0].url) {
+                  console.warn(colors.green('test /startRecord: ✔'));
+                  console.log(colors.yellow(res['clips'][0].url));
+                  try {
+                    await downloadFile(res['clips'][0].url);
+                  } catch (e) {
+                    console.log(colors.red('Download Failed'));
+                    return;
+                  }
+                } else {
+                  console.warn(colors.red('test /startRecord: ✘'));
+                }
+                resolve();
+              },
+              err => {
+                console.log(colors.red('test /startRecord:'), err);
+                reject(err);
               }
-              resolve();
-            },
-            err => {
-              console.log(colors.green("test /startRecord:"), err);
-              reject(err);
-            }
-          );
+            );
+          }, 10 * 1000);
         }
       },
       err => {
         if (DEBUG) console.error(err);
 
-        if (err == "recording_init_failed") {
+        if (err == 'recording_init_failed') {
           reject(err);
           return;
         }
@@ -88,7 +90,7 @@ function testStartRecord() {
 
 function getClips() {
   return new Promise((resolve, reject) => {
-    request(baseUrl + "getClips", { json: true }, (err, res, body) => {
+    request(baseUrl + 'getClips', { json: true }, (err, res, body) => {
       if (err) {
         console.error(err);
         reject(err);
@@ -104,7 +106,7 @@ function getClips() {
 
 function startRecord() {
   return new Promise((resolve, reject) => {
-    request(baseUrl + "startRecordSync", { json: true }, (err, res, body) => {
+    request(baseUrl + 'startRecordSync', { json: true }, (err, res, body) => {
       if (err) {
         console.error(err);
         reject(err);
@@ -123,32 +125,32 @@ function startRecord() {
 function downloadFile(url) {
   return new Promise((resolve, reject) => {
     var file = fs.createWriteStream(
-      process.env.PWD + "/output/" + url.split("/").pop()
+      process.env.PWD + '/output/' + url.split('/').pop()
     );
     var request = http
       .get(url, function(response) {
         response.pipe(file);
-        file.on("finish", function() {
-          if (DEBUG) console.log("Download complete");
+        file.on('finish', function() {
+          if (DEBUG) console.log('Download complete');
           file.close();
           resolve();
         });
       })
-      .on("error", function(err) {
+      .on('error', function(err) {
         // Handle errors
-        if (DEBUG) console.log(colors.red("Download error", err));
-        fs.unlink(process.env.PWD + "/output/" + url.split("/").pop()); // Delete the file async. (But we don't check the result)
+        if (DEBUG) console.log(colors.red('Download error', err));
+        fs.unlink(process.env.PWD + '/output/' + url.split('/').pop()); // Delete the file async. (But we don't check the result)
         reject(err);
       });
   });
 }
 
 function clearOutPutDir() {
-  fs.readdir(process.env.PWD + "/output/", (err, files) => {
+  fs.readdir(process.env.PWD + '/output/', (err, files) => {
     if (err) throw err;
 
     for (const file of files) {
-      fs.unlink(path.join(process.env.PWD + "/output/", file), err => {
+      fs.unlink(path.join(process.env.PWD + '/output/', file), err => {
         if (err) throw err;
       });
     }
